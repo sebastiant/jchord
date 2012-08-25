9069e7e96f146785b37793e316573c18dc24e236
sebastiant <sebastian.tunstig@gmail.com>
2012-08-25 16:57:06 +0200

    x:

--- src/connection/SocketTest.java
+++ src/connection/SocketTest.java
@@ -1,15 +1,22 @@
 package connection;
 
+<<<<<<< HEAD
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+=======
+import java.net.InetAddress;
+>>>>>>> bf4bc5ac3c08ba0913d4b8b968ac6fd9c96d80ab
 import java.net.UnknownHostException;
 
 public class SocketTest implements ConnectionCallback {
 
 	private ConnectionListener cl;
+	private ConnectionTable table = new ConnectionTable();
+	
 	public SocketTest(){
+<<<<<<< HEAD
 		cl = new ConnectionListener(8089, this);
 		try {
 			Connection c = new Connection(new Socket("localhost",8089),this);
@@ -23,15 +30,25 @@ public class SocketTest implements ConnectionCallback {
 			e.printStackTrace();
 		}
 		
+=======
+		cl = new ConnectionListener(8080, this, table);
+>>>>>>> bf4bc5ac3c08ba0913d4b8b968ac6fd9c96d80ab
 	}
+	
 	@Override
 	public void receive(Message msg) {
-		System.out.println("received: " + msg.getMsg() + "from: " + msg.getAddr().toString() + ":"+msg.getPort());
-		
+		System.out.println("received: " + msg.getMsg() + " from: " + msg.getAddr().toString() + ":"+msg.getPort());
 	}
 	
 	public static void main(String args[]){
 		SocketTest s = new SocketTest();
+		try {
+			Connection c = new Connection(InetAddress.getLocalHost(), 8080, null);
+			c.send("HEJ");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
