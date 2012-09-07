e74e0cb2fc8f2013e65e12bac96b915beba1a747
Jakob Stengard <jakob@n182-p215.kthopen.kth.se>
2012-09-07 17:57:39 +0200

    lol

--- src/overlay/FingerTable.java
+++ src/overlay/FingerTable.java
@@ -1,5 +1,18 @@
 package overlay;
 
+import java.math.BigInteger;
+
 public class FingerTable {
 
+	public static boolean inBetween(long start, long end, long number, long overlaySize) {		
+		long shiftedNumber = number-start;
+		long shiftedEnd = end-start;
+		
+		if(shiftedNumber > 0 && shiftedNumber <= shiftedEnd) {
+			// Number is in between start and end.
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
