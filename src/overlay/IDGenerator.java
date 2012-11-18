54de0604c6e925484e4346635b94304b62c45c9c
jakob <djgreiz@gmail.com>
2012-11-18 16:04:59 +0100

    Changed it generator to static from singleton.

--- src/overlay/IDGenerator.java
+++ src/overlay/IDGenerator.java
@@ -6,29 +6,18 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class IDGenerator {
-	
-	private static IDGenerator instance = null;
-	private MessageDigest md = null;
-	
-	private IDGenerator() throws NoSuchAlgorithmException {
-		md = MessageDigest.getInstance("SHA-256");
-	}
-
-	public long getId(InetAddress ip, int port, long overlaySize) {
+	public static long getId(InetAddress ip, int port, long overlaySize) {
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		BigInteger hash = null;
 		byte[] raw = md.digest((ip.toString() + ":" + Integer.toHexString(port)).getBytes());
 		hash = new BigInteger(1, raw);
 		return hash.mod(new BigInteger(Long.toString(overlaySize))).longValue();
 	}
-	
-	public static IDGenerator getInstance() {
-		if(instance == null) {
-			try {
-				instance = new IDGenerator();
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-			} 
-		}
-		return instance;
-	}
+
 }
