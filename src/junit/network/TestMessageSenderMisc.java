68415635891e942bfc2be54a1705b093da0e9bfa
sebastiant <sebastian.tunstig@gmail.com>
2013-01-29 14:34:28 +0100

    Added @Before and @After. Pretty code <3

--- src/junit/network/TestMessageSenderMisc.java
+++ src/junit/network/TestMessageSenderMisc.java
@@ -17,11 +17,17 @@ import org.junit.Test;
 public class TestMessageSenderMisc {
 	
 	boolean startRcvMsg;
+	MessageSender node1, node2, node3, node4, node5, node6, node7, node8, node9;
+	@Before
+	public void setUp() {
+		node1 = new MessageSender(9401);
+		node2 = new MessageSender(9402);
+		node3 = new MessageSender(9403);
+		node4 = new MessageSender(9404);
+	}
 
 	@Test
 	public void testStart() {
-		MessageSender node1 = new MessageSender(9401);
-		MessageSender node2 = new MessageSender(9402);
 		startRcvMsg = false;
 		node2.registerMessageObserver(new ConcreteObserver<Message>() {
 			@Override
@@ -39,11 +45,6 @@ public class TestMessageSenderMisc {
 		message.setDestinationAddress(node2.getAddress());
 		// send before started
 		assertFalse(node1.send(message));	
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e1) {
-			fail(e1.getMessage());
-		}
 		assertFalse(startRcvMsg);
 		node1.start();
 		assertTrue(node1.send(message));
@@ -53,14 +54,10 @@ public class TestMessageSenderMisc {
 			fail(e1.getMessage());
 		}
 		assertTrue(startRcvMsg);
-		node1.stop();
-		node2.stop();
 	}
 	
 	@Test
 	public void testDisconnect() {
-		MessageSender node1 = new MessageSender(9503);
-		MessageSender node2 = new MessageSender(9504);
 		node1.start();
 		node2.start();
 		Message msg = new Message();
@@ -86,17 +83,10 @@ public class TestMessageSenderMisc {
 		}
 		//assertTrue(node2.getConnections().size() == 0);
 		assertFalse(node1.disconnect(addr));
-		node1.stop();
-		node2.stop();
 	}
-
 
 	@Test
 	public void testGetConnections() {
-		MessageSender node1 = new MessageSender(9605);
-		MessageSender node2 = new MessageSender(9606);
-		MessageSender node3 = new MessageSender(9607);
-		MessageSender node4 = new MessageSender(9608);
 		node1.start();
 		node2.start();
 		node3.start();
@@ -112,28 +102,32 @@ public class TestMessageSenderMisc {
 			fail(e1.getMessage());
 		}
 		assertTrue(node1.getConnections().size() == 3);
-		
 		assertTrue(node1.getConnections().containsKey(node2.getAddress()));
 		assertTrue(node1.getConnections().containsKey(node3.getAddress()));
 		assertTrue(node1.getConnections().containsKey(node4.getAddress()));
-		
+
 		assertTrue(node2.getConnections().containsKey(node1.getAddress()));
 		assertEquals(node2.getConnections().get(node1.getAddress()).getAddress(),node1.getAddress());
-		node1.stop();
-		node2.stop();
-		node3.stop();
-		node4.stop();
+
 	}
 
 	@Test
 	public void testGetAddress() {
-		MessageSender node1 = new MessageSender(9709);
-		assertTrue(node1.getAddress().getPort() == 9709);
+		assertTrue(node1.getAddress().getPort() == 9401);
 		try {
 			assertEquals(node1.getAddress().getInetAddress(), InetAddress.getLocalHost());
 		} catch (UnknownHostException e) {
 			fail(e.getMessage());
 		}
+	}
+	
+	@After
+	public void cleanUp() {
+		node1.stop();
+		node2.stop();
+		node3.stop();
+		node4.stop();
+		node1 = node2 = node3 = node4 = null;
 	}
 
 }
