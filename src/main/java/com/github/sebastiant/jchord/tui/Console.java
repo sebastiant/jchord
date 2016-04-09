050c402cd2bd23070b54d71f81ba360ac0fa1ca6
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2016-04-09 18:20:57 +0200

    add gradle. gradle manage deps. move Main from own class

--- src/main/java/com/github/sebastiant/jchord/tui/Console.java
+++ src/main/java/com/github/sebastiant/jchord/tui/Console.java
@@ -160,4 +160,24 @@ public class Console {
 		}
 	}
 
+	public static void main(String args[]) {
+		System.out.println("DHT User Interface");
+		int port = 8000;
+		int arity = 2;
+		long idSpace = (long) Math.pow(2, 62);
+
+		if(args.length >= 1) {
+			port = Integer.parseInt(args[0]);
+		}
+		if(args.length >= 2) {
+			arity = Integer.parseInt(args[1]);
+		}
+		if(args.length >= 3) {
+			idSpace = Long.parseLong(args[2]);
+		}
+		System.out.println("Starting with port: " + port + ", arity: " + arity);
+		Console cons = new Console(port, idSpace, arity);
+		cons.run();
+	}
+
 }
